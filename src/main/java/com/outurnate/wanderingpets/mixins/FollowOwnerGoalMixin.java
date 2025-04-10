24107561cea9aa28d53605683c41ee663d6d99dd
wnyyyy <viniberizzi@gmail.com>
2025-04-09 22:41:09 -0300

    fix dirty code

--- src/main/java/com/outurnate/wanderingpets/mixins/FollowOwnerGoalMixin.java
+++ src/main/java/com/outurnate/wanderingpets/mixins/FollowOwnerGoalMixin.java
@@ -1,23 +1,16 @@
 package com.outurnate.wanderingpets.mixins;
 
 import com.outurnate.wanderingpets.Config;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.ai.goal.FollowOwnerGoal;
 import net.minecraft.world.entity.ai.goal.Goal;
-import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.Inject;
 import com.outurnate.wanderingpets.interfaces.IFollowsAccessor;
-
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import javax.annotation.Nullable;
 
 @Mixin(FollowOwnerGoal.class)
 public abstract class FollowOwnerGoalMixin extends Goal
@@ -26,12 +19,6 @@ public abstract class FollowOwnerGoalMixin extends Goal
     @Final
     @Shadow
     private TamableAnimal tamable;
-
-    @Shadow public abstract void stop();
-
-    @Shadow public abstract boolean canUse();
-
-    @Shadow @Nullable private LivingEntity owner;
 
     @Inject(at = @At("HEAD"), method = "canContinueToUse", cancellable = true)
     public void canContinueToUse(CallbackInfoReturnable<Boolean> cir)
