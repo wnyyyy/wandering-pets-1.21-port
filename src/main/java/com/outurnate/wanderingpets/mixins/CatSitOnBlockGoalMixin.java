b72b8ebfd67bcd7eeb7e0eb83355a10bf6bc2814
wnyyyy <viniberizzi@gmail.com>
2025-04-20 00:12:30 -0300

    code clean up

--- src/main/java/com/outurnate/wanderingpets/mixins/CatSitOnBlockGoalMixin.java
+++
@@ -1,9 +0,0 @@
-package com.outurnate.wanderingpets.mixins;
-
-import net.minecraft.world.entity.ai.goal.CatSitOnBlockGoal;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(CatSitOnBlockGoal.class)
-public abstract class CatSitOnBlockGoalMixin {
-
-}
--- src/main/java/com/outurnate/wanderingpets/mixins/FollowOwnerGoalMixin.java
+++ src/main/java/com/outurnate/wanderingpets/mixins/FollowOwnerGoalMixin.java
@@ -14,6 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(FollowOwnerGoal.class)
 public abstract class FollowOwnerGoalMixin extends Goal {
+
     @Final
     @Shadow
     private TamableAnimal tamable;
