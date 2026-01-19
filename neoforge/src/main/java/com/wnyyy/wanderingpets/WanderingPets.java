b5f256877cee352296312a7c0710d5b5133b5efe
wnyyyy <viniberizzi@gmail.com>
2026-01-19 14:11:12 -0300

    1.20.4

--- neoforge/src/main/java/com/wnyyy/wanderingpets/WanderingPets.java
+++ neoforge/src/main/java/com/wnyyy/wanderingpets/WanderingPets.java
@@ -1,7 +1,7 @@
 package com.wnyyy.wanderingpets;
 
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.config.ModConfigEvent;
@@ -9,8 +9,8 @@ import net.neoforged.fml.event.config.ModConfigEvent;
 @Mod(Constants.MOD_ID)
 public class WanderingPets {
 
-    public WanderingPets(IEventBus modEventBus, ModContainer modContainer) {
-        modContainer.registerConfig(ModConfig.Type.COMMON, NeoForgeModConfig.SPEC);
+    public WanderingPets(IEventBus modEventBus) {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, NeoForgeModConfig.SPEC);
         modEventBus.addListener(this::onConfigLoad);
 
         WanderingPetsCommon.init();
